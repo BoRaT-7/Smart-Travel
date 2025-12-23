6023e349fa8a81a4a4af97063cb60a7f0b4a7264
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-12-23 23:23:41 +0600

    commit-3

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
+// src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import Router from "./Routers/Router";
-import Authprovider from "./provider/Authprovider";
+import AuthProvider from "./provider/AuthProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Authprovider>
+    <AuthProvider>
       <RouterProvider router={Router} />
-    </Authprovider>
+    </AuthProvider>
   </StrictMode>
 );
