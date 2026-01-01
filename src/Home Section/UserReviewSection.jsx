e151e64e42de982d738e9e6b9f1addc20e6cfa2a
Md Bocktear Abid Borat <boratabid@gmail.com>
2026-01-01 22:30:41 +0600

    fixed

--- src/Home Section/UserReviewSection.jsx
+++ src/Home Section/UserReviewSection.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
 import { FaStar, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { motion } from "framer-motion";
-import { AuthContext } from "../provider/Authprovider"; // adjust path
+import AuthProvider from "../provider/Authprovider";
 
 const UserReviewSection = () => {
   const { user } = useContext(AuthContext);
